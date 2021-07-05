335a1aef8b941d2ae95ad1da60222bb490189ab0
buyan7 <bkhishig1@gmail.com>
2021-07-05 18:05:46 +0800

    zasvar

--- src/index.js
+++ src/index.js
@@ -16,13 +16,16 @@ const store = createStore(reducer);
 
 const options = {
   // you can also just use 'bottom center'
-  position: positions.MIDDLE,
+  position: positions.TOP_CENTER,
   timeout: 1000,
   offset: "30px",
   containerStyle: { color: "#233772" },
 
   // you can also just use 'scale'
   transition: transitions.SCALE,
+  containerStyle: {
+    zIndex: 100,
+  },
 };
 
 ReactDOM.render(
